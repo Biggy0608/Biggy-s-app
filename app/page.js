d01f470a6fe8b2f32a8f94989f9e44a9c054df4b
Biggy0608 <jonathan.maria0608@gmail.com>
2025-07-12 14:32:30 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenue sur Biggy's App !</h1>
+      <p>Ta collection de jeux vidéo arrive bientôt ici 🎮</p>
+    </main>
+  );
+}
